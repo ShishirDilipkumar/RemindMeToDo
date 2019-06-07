faec2ba32891757ebf2e7f13919a67f56ac85a39
Shishir Dilipkumar <sdilipku@in.ibm.com>
2019-06-07 18:06:47 +0530

    Changes to display items on tableview + initial setup

---
+++ RemindMeToDo/ToDoViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  RemindMeToDo
+//
+//  Created by Shishir Dilipkumar on 07/06/19.
+//  Copyright © 2019 Shishir Dilipkumar. All rights reserved.
+//
+
+import UIKit
+
+class ToDoViewController: UITableViewController {
+
+    let itemArray = ["Item1","Item2","Item3"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+       // tableView.register(self, forCellReuseIdentifier: "toDoCell")
+        //tableView.dataSource = self
+    }
+    
+    //MARK: - Tableview Datasource methods
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "toDoCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    //MARK: - TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+             tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
